3ebc1cfbd78d96274821ba6a4dd138e6c63065b0
Daniel-YH-Cai <79785628+Daniel-YH-Cai@users.noreply.github.com>
2022-10-18 20:33:53 +0800

    Update NgramCount.java

--- NgramCount.java
+++ NgramCount.java
@@ -5,42 +5,122 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.util.*;
 
 public class NgramCount {
 
-    private static class NGCMapper extends Mapper<Object, Text,Text, IntWritable> {
-        private static int N;
-
-        @Override
-        protected void setup(Context context) throws IOException, InterruptedException {
-            N=Integer.parseInt(context.getConfiguration().get("N"));
-        }
-
-        @Override
-        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+//    private static class NGCMapper extends Mapper<Object, Text,Text, IntWritable> {
+//        private static int N;
+//        private static ArrayList<String> lastMapWords;
+//
+//        @Override
+//        protected void setup(Context context) throws IOException, InterruptedException {
+//            lastMapWords=new ArrayList<>();
+//            N=Integer.parseInt(context.getConfiguration().get("N"));
+//        }
+//
+//        @Override
+//        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+//            String[] tokens=value.toString().split("\\W+");
+//            if(tokens.length==0){
+//                return;
+//            }
+//            int startIndex="".equals(tokens[0])?1:0;
+//            int cachedSize=lastMapWords.size();
+//            int newLength=cachedSize+tokens.length-startIndex;
+//            if(newLength<N){
+//                lastMapWords.addAll(Arrays.asList(tokens).subList(startIndex, tokens.length));
+//                return;
+//            }
+//            else{
+//                for(int i=0;i<lastMapWords.size();i++){
+//                    StringBuilder sb=new StringBuilder();
+//                    sb.append(lastMapWords.get(i));
+//                    for(int j=1;j<=N-1;i++){
+//                        sb.append(' ');
+//                        if(j<lastMapWords.size()){ ;
+//                            sb.append(lastMapWords.get(j));
+//                        }
+//                        else{
+//                            sb.append(tokens[j+startIndex-lastMapWords.size()]);
+//                        }
+//                    }
+//                    context.write(new Text(sb.toString()),new IntWritable(1));
+//                }
+//                for(int i=startIndex;i<tokens.length-N+1;i++){
+//                    StringBuilder sb=new StringBuilder();
+//                    sb.append(tokens[i]);
+//                    for(int j=1;j<=N-1;j++){
+//                        sb.append(' ');
+//                        sb.append(tokens[j+i]);
+//                    }
+//                    context.write(new Text(sb.toString()),new IntWritable(1));
+//                }
+//                for(int i=lastMapWords.size();i<N-1;i++){
+//                    lastMapWords.add("");
+//                }
+//                for(int i=1;i<=N-1;i++){
+//                    int indexInToken=tokens.length-N+i;
+//                    if(indexInToken<0){
+//                        lastMapWords.set(i-1,lastMapWords.get(lastMapWords.size()+indexInToken));
+//                    }
+//                    else{
+//                        lastMapWords.set(i-1,tokens[indexInToken]);
+//                    }
+//                }
+//
+//            }
+//
+//            //adding the last N-1 words to the deque
+//        }
+//    }
+    private static class NGCMapper extends Mapper<Object, Text,Text, IntWritable>{
+    private static final IntWritable ONE=new IntWritable(1);
+    @Override
+    public void run(Context context) throws IOException, InterruptedException {
+        int N=Integer.parseInt(context.getConfiguration().get("N"));
+        Deque<String> lastN=new ArrayDeque<>();
+        while(context.nextKeyValue()){
+            Text value=context.getCurrentValue();
             String[] tokens=value.toString().split("\\W+");
             if(tokens.length==0){
-                return;
+                continue;
             }
             int startIndex="".equals(tokens[0])?1:0;
-            for(int i=startIndex;i<=tokens.length-N;i++){
-                StringBuilder sb=new StringBuilder();
-                for(int j=i;j<=i+N-1;j++){
-                    sb.append(tokens[j]);
-                    if(j<i+N-1){
-                        sb.append(" ");
-                    }
-                }
-                context.write(new Text(sb.toString()),new IntWritable(1));
+            while (startIndex<tokens.length&&lastN.size()<N){
+                lastN.offerLast(tokens[startIndex]);
+                startIndex++;
             }
+            for(int i=startIndex;i<tokens.length;i++){
+                context.write(toNgram(lastN),ONE);
+                lastN.pollFirst();
+                lastN.offerLast(tokens[i]);
+            }
+
         }
+        context.write(toNgram(lastN),ONE);
     }
+    private static Text toNgram(Deque<String> d){
+        Iterator<String> itr=d.iterator();
+        StringBuilder sb=new StringBuilder();
+        if(itr.hasNext()){
+            sb.append(itr.next());
+        }
+        while (itr.hasNext()){
+            sb.append(' ');
+            sb.append(itr.next());
+        }
+        return new Text(sb.toString());
+    }
+}
     private static class NGCReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
         @Override
         protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
@@ -49,6 +129,12 @@ public class NgramCount {
                 sum=sum+iw.get();
             }
             context.write(key,new IntWritable(sum));
+        }
+    }
+    private static class NGCTextInputFormat extends TextInputFormat{
+        @Override
+        protected boolean isSplitable(JobContext context, Path file) {
+            return false;
         }
     }
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
@@ -65,6 +151,7 @@ public class NgramCount {
         job.setCombinerClass(NgramCount.NGCReducer.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setInputFormatClass(NgramCount.NGCTextInputFormat.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
